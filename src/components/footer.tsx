d092298448ee7c73f1a2e49667e729ef480d61ba
sadegh <mrsadeghsn@gmail.com>
2023-04-09 21:06:09 +0430

    Init

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <>
+    <h1 className='text-xxl'>Footer</h1>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/home.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import Header from "./header";
+import Footer from "./footer";
+
+function Home() {
+	return (
+		<>
+			<Header />
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Footer />
+		</>
+	);
+}
+
+export default Home;
